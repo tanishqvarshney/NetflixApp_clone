c92bf8f351878e9b65e09eac71f24249fb1d1e6e
Tanishq Varshney <tanishqvarshney@Tanishqs-MacBook-Air.local>
2025-08-13 12:18:24 +0530

    Revert "files"
    This reverts commit ff8024478a09983325bd4ff4700dbe640d7ac68d.

--- server/middleware/subscription.js
+++
@@ -1,7 +0,0 @@
-exports.requireSubscription = (req, res, next) => {
-  if (req.user && req.user.isSubscribed) {
-    next();
-  } else {
-    res.status(402).json({ message: 'Subscription required' });
-  }
-};
